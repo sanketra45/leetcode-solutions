f0284cbab73440e8ee76e7e63777763627687cf0
Sanket Rathod <sanketra53@gmail.com>
2026-07-26 23:48:00 +0530

    Time: 5 ms (67.5%), Space: 46.9 MB (84.42%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+
+        // BHAI IS PROBLEM ME MIN OPERATIONS TO MATCH BOTH STRINGS CALCULATE KRNA HAI
+        // HAM EK DP ARRAY BANAYENGE AND DP KE HAR ELEMENT PAR USSE PAHLE TAK STRINGS KO MATCH KRENE KITNE OPN LAGENGE YE STORE KRENGE
+
+        int m = word1.length(), n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i = 1; i <= m; i++)
+        {
+            dp[i][0] = i;
+        }
+
+        for(int j = 1; j <= n; j++)
+        {
+            dp[0][j] = j;
+        }
+
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1))
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                else{
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
