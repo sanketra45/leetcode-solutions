35c50b8b4d0c5a0dab69f573fe9f1a9021106a5a
Sanket Rathod <sanketra53@gmail.com>
2026-07-31 00:11:17 +0530

    Time: 2 ms (98.18%), Space: 72.6 MB (29.07%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for (int num : bills) {
+            if (num == 5) {
+                five++;
+            }
+            
+            else if (num == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                }
+                
+                else {
+                    return false;
+                }
+            }
+            
+            else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                }
+                
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
