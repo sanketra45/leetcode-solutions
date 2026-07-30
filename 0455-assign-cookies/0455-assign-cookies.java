a210762545190c799fcf19277f1b1d9096836eb7
Sanket Rathod <sanketra53@gmail.com>
2026-07-30 23:37:45 +0530

    Time: 15 ms (60.03%), Space: 51.6 MB (37.02%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int left = 0, right = 0;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(left < s.length && right < g.length)
+        {
+            if(g[right] <= s[left])
+            {
+                right++;
+            }
+            left++;
+        }
+
+        return right;
+    }
+}
